96d66d7dcf8db846dab2a1a77956c1726148ead8
Rob Head <3846814+Rob89@users.noreply.github.com>
2024-03-28 11:56:31 +0000

    fix building on windows

--- build.rs
+++ build.rs
@@ -5,10 +5,20 @@ fn main() {
     let input = format!("{dir}/src/ui/style.css");
     let output = format!("{dir}/assets/style.css");
 
-    let result = std::process::Command::new("npx")
-        .args(["--yes", "tailwindcss", "-i", &input, "-o", &output])
-        .output()
-        .expect("Unable to generate css");
+    let result: Output;
+
+    if cfg!(windows) {
+        let cmd = format!("tailwindcss --yes -i {} -o {}", input, output);
+        result = std::process::Command::new("cmd")
+            .args(["/C", &cmd])
+            .output()
+            .expect("Unable to generate css");
+    } else {
+        result = std::process::Command::new("npx")
+            .args(["--yes", "tailwindcss", "-i", &input, "-o", &output])
+            .output()
+            .expect("Unable to generate css");
+	}
 
     if !result.status.success() {
         let error = String::from_utf8_lossy(&result.stderr);
